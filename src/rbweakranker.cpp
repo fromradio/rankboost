bc0a41b90cd190221cf60ee1ab9942124b5b9660
fromradio <ruimin.wang13@gmail.com>
2015-11-03 10:45:52 +0800

    rankboost weak ranker

---
+++ src/rbweakranker.cpp
@@ -0,0 +1,49 @@
+// Copyright 2015 Ruimin Wang
+// Author: Ruimin Wang, wangruimin_sx@qiyi.com, ruimin.wang13@gmail.com
+
+#include "rbweakranker.h"
+
+RBWeakRanker::RBWeakRanker()
+	: __fid(-1), __threshold(-1.0)
+{
+}
+
+RBWeakRanker::RBWeakRanker(size_t fid, double threshold)
+	: __fid(fid), __threshold(threshold)
+{
+}
+
+RBWeakRanker::RBWeakRanker(const RBWeakRanker& ranker)
+	: __fid(ranker.fid()), __threshold(ranker.threshold())
+{
+}
+
+double RBWeakRanker::score(const Sample& s)
+{
+	if(__fid==-1)
+		return 0.;
+	if(s.getFeature(__fid)>__threshold)
+		return 1.;
+	else
+		return 0.;
+}
+
+size_t RBWeakRanker::fid() const
+{
+	return __fid;
+}
+
+void RBWeakRanker::setFid(size_t fid)
+{
+	__fid=f;
+}
+
+double RBWeakRanker::threshold() const
+{
+	return __threshold;
+}
+
+void RBWeakRanker::setThreshold(double t)
+{
+	__threshold=t;
+}
---
+++ src/rbweakranker.h
@@ -0,0 +1,50 @@
+#ifndef RBWEAK_RANKER_H__
+#define RBWEAK_RANKER_H__
+
+//	Weaker ranker for rankboost algorithm
+//		the ranker classify given sample according to a threshold
+//	along a specified feature. in the ranker we stored fid which
+//	is the index of the feature and the corresponding threshold.
+//	we can score a given sample given fid and threshold
+#include <cstddef>
+
+#include "sample.h"
+
+class RBWeakRanker
+{
+public:
+	// default constructor, fid is set as -1, threshold is set as 0.
+	RBWeakRanker();
+
+	// constructor setting fid and threshold
+	RBWeakRanker(size_t fid, double threshold);
+
+	// copy constructor
+	RBWeakRanker(const RBWeakRanker& ranker);
+
+	// score a given sample, 0. is returned if fid equals to -1
+	double score(const Sample& s) const;
+
+	// getter and setter of fid
+	size_t fid() const;
+	void setFid(size_t);
+
+	// getter and setter of threshold
+	double threshold() const;
+	void setThreshold(double t);
+
+private:
+
+	// the index of feature
+	size_t __fid;
+
+	// the threshold, the ranker will give a 1.0 score if the value
+	// of the corresponding feature is larger than threshold
+	double __threshold;
+
+
+	double __q; 	// TODO
+};
+
+
+#endif
